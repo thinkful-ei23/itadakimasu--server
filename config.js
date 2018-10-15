7c32df22e7c0c542fbc70ca793209b3fea6d5ec4
Tarik Desire <35511872+tdesire@users.noreply.github.com>
2018-10-15 14:57:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/itadakimasu-test',
+  JWT_SECRET : process.env.JWT_SECRET,
   JWT_EXPIRY : process.env.JWT_EXPIRY || '7d'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
