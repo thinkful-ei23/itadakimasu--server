654c72ef2845240ba8f86394f668e2deb3662de7
Tarik Desire <tprdesire@gmail.com>
2018-10-15 12:28:40 -0400

    Added passport local strategy, seeded db with sample users

---
+++ utils/seed-database.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { DATABASE_URL } = require('../config');
+const User = require('../models/user');
+
+const seedUsers = require('../db/seed/user');
+
+mongoose.connect(DATABASE_URL)
+  .then(() => mongoose.connection.db.dropDatabase())
+  .then(() => {
+    return Promise.all([
+      User.insertMany(seedUsers),
+      User.createIndexes()
+    ]);
+  })
+  .then(() => mongoose.disconnect())
+  .catch(err => {
+    console.error(err);
+  });
