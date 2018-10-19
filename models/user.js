ae17631a97c49d5e814588d9aff0f8231f4c0104
Burkey Koontz <bk.koontz@gmail.com>
2018-10-19 09:43:52 -0700

    alt is added to pictures

--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   questions: [
     {
       _id: mongoose.Schema.Types.ObjectId,
+      imageDescription: String,
       imageURL: String,
       question: String,
       answer: String,
